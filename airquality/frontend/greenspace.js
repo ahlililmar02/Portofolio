b31703f680d9977dea4aabbb208b236d6a47c82a
ahlililmar02 <136542395+ahlililmar02@users.noreply.github.com>
2025-12-02 21:54:55 +0700

    modified script

--- airquality/frontend/greenspace.js
+++ airquality/frontend/greenspace.js
@@ -109,8 +109,8 @@ if (mapElement && typeof L !== 'undefined') {
     cities.forEach((city) => {
         const divIcon = L.divIcon({
             className: "custom-marker",
-            html: `<div style="background: white; height:10px; width:40px; display:flex;text-align: center;
-                    padding: 4px 8px; border-radius: 4px; border: 2px solid #f59e0b; font-size: 12px; font-weight: 500; 
+            html: `<div style="background: white; height:10px; width:50px; display:flex;text-align: center;
+                    padding: 2px 3px; border-radius: 4px; border: 2px solid #f59e0b; font-size: 10px; font-weight: 500; 
                     color: #92400e; white-space: nowrap; box-shadow: 0 2px 4px rgba(0,0,0,0.2); cursor: pointer;">${city.name}</div>`,
             iconSize: [0, 0],
             iconAnchor: [0, 0],
