57c3693ad136be936f0650b1f47f4dca45b86535
ahlililmar02 <136542395+ahlililmar02@users.noreply.github.com>
2025-12-10 09:50:33 +0700

    edit main

--- airquality/backend/main.py
+++ airquality/backend/main.py
@@ -102,6 +102,7 @@ def get_all_latest():
                     aqi.latitude != 'NaN' AND
                     aqi.longitude != 'NaN' AND
                     aqi.aqi != 'NaN' AND
+                    aqi.aqi !=  0
                     aqi.pm25 != 'NaN'
                 ORDER BY aqi.station, aqi.time DESC;
             """)
